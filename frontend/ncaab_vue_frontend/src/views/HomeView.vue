46772e985368bc2d32dd703f8560b2ecde5d72f3
joe racaniello <jvr2424@gmail.com>
2022-03-07 00:41:32 -0500

    adding updated home view

--- frontend/ncaab_vue_frontend/src/views/HomeView.vue
+++ frontend/ncaab_vue_frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+import FullRankings from "@/components/FullRankings.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <FullRankings />
   </main>
 </template>
+
