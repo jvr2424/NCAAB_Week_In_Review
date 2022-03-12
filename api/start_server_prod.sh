42e6474eeee3773fed4e337d1c5690493290a226
joe racaniello <jvr2424@gmail.com>
2022-03-12 12:24:02 -0500

    updating cron path

--- api/start_server_prod.sh
+++ api/start_server_prod.sh
@@ -13,7 +13,7 @@ apt-get update && apt-get upgrade -y && apt-get install cron -y
 
 
 # load the db at 3 am EST every night (+5 UTC)
-echo "0 8 * * * cd /ncaab/ncaab/top_teams_week/load_data && python collect_data_main.py" > cron_schedule
+echo "0 8 * * * cd /usr/app/venv/ncaab/top_teams_week/load_data && python collect_data_main.py" > cron_schedule
 
 crontab cron_schedule
 cron
